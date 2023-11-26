a6f6a855a4e18d798abf2e084232a96037d49b9e
iRewiewer <irewiewer@gmail.com>
2023-11-26 23:26:55 +0200

    Implemented v1.0

--- main.ts
+++ main.ts
@@ -1,134 +1,127 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+    seedValue: number;
+    lowRange: number;
+    highRange: number;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+    seedValue: Math.floor(Math.random() * (10 ** 12)),
+    lowRange: 1,
+    highRange: 100,
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        console.log("Loading Random Number Generator")
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // This adds a simple command that can be triggered anywhere
+        this.addCommand({
+            id: 'random-int',
+            name: `Generate a random integer. Range can be modified in settings.`,
+            callback: async () => {
+                // Generate a random number
+                const seed = parseFloat(`0.${this.settings.seedValue.toString()}`);
+                const RNG = (Math.random() + seed) % 1;
+                const randomNumber: number = Math.floor(RNG * this.settings.highRange + this.settings.lowRange);
+                const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+                if (activeView) {
+                    const editor = activeView.editor;
+                    const cursorPos = editor.getCursor();
+                    if (cursorPos) {
+                        editor.replaceRange(`${randomNumber} `, cursorPos);
+                        editor.setCursor(cursorPos.line, cursorPos.ch + randomNumber.toString().length + 1);
+                    }
+                }
+            }
+        });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new SettingsTab(this.app, this));
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingsTab extends PluginSettingTab {
+    plugin: MyPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: MyPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        new Setting(containerEl)
+            .setName('Seed')
+            .setDesc('Leave empty for a random one. (Default: secret)')
+            .addText(text => text
+                .setPlaceholder('Enter seed')
+                .setValue(this.plugin.settings.seedValue.toString())
+                .onChange(async (value) => {
+                    // replace any non digit with ascii value modulo 1 trillion
+                    this.plugin.settings.seedValue = parseInt(value.replace(/\D/g, (match) => match.charCodeAt(0).toString())) % (10 ** 12);
+                    if (isNaN(this.plugin.settings.seedValue)) {
+                        this.plugin.settings.seedValue = 0;
+                    }
+                    await this.plugin.saveSettings();
+                }));
 
-	display(): void {
-		const {containerEl} = this;
+        new Setting(containerEl)
+            .setName('Low range value')
+            .setDesc('Set the low point of the random range. (Default: 1)')
+            .addText(text => text
+                .setPlaceholder('Enter number')
+                .setValue(this.plugin.settings.lowRange.toString())
+                .onChange(async (value) => {
+                    // replace any non digit with ascii value modulo 1 trillion
+                    this.plugin.settings.lowRange = parseInt(value.replace(/\D/g, ''));
+                    text.setValue(this.plugin.settings.lowRange.toString());
+                    if (isNaN(this.plugin.settings.seedValue)) {
+                        this.plugin.settings.seedValue = 1;
+                    }
+                    if (text.getValue() == "NaN") {
+                        text.setValue("");
+                    }
+                    await this.plugin.saveSettings();
+                }));
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('High range value')
+            .setDesc('Set the high point of the random range. (Default: 100)')
+            .addText(text => text
+                .setPlaceholder('Enter number')
+                .setValue(this.plugin.settings.highRange.toString())
+                .onChange(async (value) => {
+                    // replace any non digit with ascii value modulo 1 trillion
+                    this.plugin.settings.highRange = parseInt(value.replace(/\D/g, ''));
+                    text.setValue(this.plugin.settings.highRange.toString());
+                    if (isNaN(this.plugin.settings.seedValue)) {
+                        this.plugin.settings.seedValue = 100;
+                    }
+                    if (text.getValue() == "NaN") {
+                        text.setValue("");
+                    }
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
